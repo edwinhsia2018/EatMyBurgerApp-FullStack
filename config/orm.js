790ac4b8fed1fafe4858ccbcb2a3c63b5bf34672
Edwin Hsia <edwinhsia@gmail.com>
2018-04-13 00:16:13 -0500

    Thursday Updates

---
+++ config/orm.js
@@ -0,0 +1,7 @@
+var connections = require("../config/connection.js");
+
+connections.query("SELECT *")
+
+connections.query("INSERT INTO ")
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+("California Burger", 0),
+("PBJ Burger", 0),
+("DA BOMB Burger", 0),
+("Bacon Burger", 0);
